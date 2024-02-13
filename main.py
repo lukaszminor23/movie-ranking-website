9dcd0879dd2dbf5aeb5084826844a0edc6e4b9b9
Łukasz Minor <lukaszminor23@gmail.com>
2024-02-13 16:06:24 +0100

    created database for movies

--- main.py
+++ main.py
@@ -13,15 +13,42 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
+
 # CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 
 # CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(String[500], nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+# with app.app_context():
+#     result = db.session.execute(db.select(Movie).order_by(Movie.rating))
+#     all_movies = result.scalars()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    query = db.session.execute(db.select(Movie).order_by(Movie.rating))
+    movies = query.scalars()
+    return render_template("index.html", data=movies)
 
 
 if __name__ == '__main__':
